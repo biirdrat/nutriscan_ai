f1a569b7c5f83f7fab20828a824d07dcbfe7fe20
Austin <abatchelor1337@gmai.com>
2024-05-27 17:41:10 -0500

    Updating docstrings build_executable.py

--- build_executable.py
+++ build_executable.py
@@ -1,8 +1,18 @@
+"""
+This module builds an executable from a Python script using PyInstaller.
+
+Imports:
+subprocess: Allows you to spawn new processes, connect to their input/output/error pipes, 
+and obtain their return codes.
+os: Provides a way of using operating system-dependent functionality,
+"""
 import subprocess
 import os
 
 def build_executable():
-    # Define the path to the main script
+    """ 
+    Define the path to the main script
+    """
     main_script_path = os.path.abspath('src/GUI/main.py')
 
     # Define the PyInstaller command
@@ -14,14 +24,14 @@ def build_executable():
         '--add-data', os.path.abspath('src/GUI/ui/mainwindow.ui') + os.pathsep + 'GUI/ui',
         main_script_path
     ]
-    
     # Run the build command
     result = subprocess.run(build_command, check=True)
-    
     if result.returncode == 0:
         print("Build succeeded!")
     else:
         print("Build failed!")
 
 if __name__ == '__main__':
+    #If this module is being run as the main program, execute the build_executable function.
     build_executable()
+    
